98ad8ad7460981756acdfa404bd95fbef0694242
Moritz <s0586508@htw-berlin.de>
2023-11-17 18:12:52 +0100

    erste Methoden angefangen.

---
+++ dbo.Vogelsammlung.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Vogelsammlung] (
+    [Id]    INT        NOT NULL IDENTITY,
+    [Vogel] NCHAR (10) NULL,
+    [Art]   NCHAR (10) NOT NULL,
+    [Datum] NCHAR (10) NOT NULL,
+    [Ort]   NCHAR (10) NOT NULL,
+    [Bild] IMAGE NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
