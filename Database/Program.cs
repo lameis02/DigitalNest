dfbec349889191eadbbb1c4c5b186c8a45a83acc
Moritz <s0586508@htw-berlin.de>
2023-11-20 22:10:00 +0100

    "Add" Methode funktioniert. (Bei mir auf jeden Fall)

--- Database/Program.cs
+++ Database/Program.cs
@@ -15,11 +15,12 @@ namespace Database
         }
         public static void Add(string Bird) //sollte vielleicht ein Objekt werden mit Eigenschaften
         {
-            SqlConnection Connection = new SqlConnection(@"Server=RANGORX\SQLEXPRESS01;Database=myDataBase;User Id=myUsername;Password=myPassword;");
+            //SqlConnection Connection = new SqlConnection(@"Server=RANGORX\SQLEXPRESS01;Database=Vogelsammlung;User Id=RangoRX\morit;Password=gfb;");
+            SqlConnection Connection = new SqlConnection("Data Source=RANGORX\\SQLEXPRESS01;Initial Catalog=Vogelsammlung;Integrated Security=True;Pooling=False;Encrypt=False;");
             Connection.Open();
             string query = "Insert Into Vogelsammlung (Id,Vogel,Art,Datum,Ort) values (@Id,@Vogel,@Art,@Datum,@Ort)";
             SqlCommand command = new SqlCommand(query, Connection);
-            command.Parameters.AddWithValue("@Id", "max.Id+1");
+            command.Parameters.AddWithValue("@Id", "");
             command.Parameters.AddWithValue("@Vogel", "Bird.Vogel");
             command.Parameters.AddWithValue("@Art", "Bird.Art");
             command.Parameters.AddWithValue("@Datum", "Bird.Datum");
