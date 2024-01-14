d5dd2ff66b8029e3d8e104bb01719161587c9514
Laura Isler <s0587072@htw-berlin.de>
2024-01-14 14:51:25 +0100

    Änderung auf Version 7.0

---
+++ WPF_UI/ViewModel/StatisticsVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF.Model;
+
+namespace WPF.ViewModel
+{
+    class StatisticsVM: Utilities.ViewModelBase
+    {
+        private readonly PageModel _pageModel;
+
+        public StatisticsVM()
+        {
+            _pageModel = new PageModel();
+        }
+    }
+}
